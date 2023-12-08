0817f6caeed709ec5583e985adf8c0f2a7aa568f
서영 김 <kkimseo0@naver.com>
2023-12-08 19:20:24 +0900

    jhgf

---
+++ CreateTexture/CWindow.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <Windows.h>
+
+class CWindow
+{
+public:
+	HWND		m_hWnd;
+	HINSTANCE	m_hInstance;
+	RECT		m_rtWindow;
+	RECT		m_rtClient;
+public:
+	void SetWindow(HINSTANCE hInstance, int nCmdShow);
+	void Run();
+	void ReleaseAll();
+public:
+	virtual bool GameInit() { return true; };
+	virtual bool GameRender() { return true; };
+	virtual bool GameRelease() { return true; };
+public:
+	BOOL InitInstance(HINSTANCE hInstance, int nCmdShow);
+	ATOM MyRegisterClass(HINSTANCE hInstance);
+};
+
